4c0b522fb6d05e534e46280c8309f4ec5a9cb8cc
Anuruddha <Anuruddha.Khare@moreyeahs.in>
2026-03-17 18:57:54 +0530

    Fix Cloud

--- api/app.py
+++ api/app.py
@@ -38,5 +38,5 @@ def predict(data: PatientData):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
